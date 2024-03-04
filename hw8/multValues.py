3a951f5d509379ad76b9a47b53d05ff5dcb03ef6
iKot <sergey.savchuk91@gmail.com>
2024-03-05 00:43:29 +0200

    fixed hw8 v2

--- hw8/multValues.py
+++ hw8/multValues.py
@@ -2,17 +2,6 @@
 # example_list = [6]
 example_list = []
 
-# result = 0
-# for i, el in enumerate(example_list):
-#     if not i % 2:
-#         result += el
-#         print(el)
-#
-# if len(example_list):
-#     result *= example_list[-1]
-#
-# print(result)
-
 result = 0
 
 if len(example_list):
