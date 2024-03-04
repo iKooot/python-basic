da3543b906de10828579e1519ac61d0002591bb7
iKot <sergey.savchuk91@gmail.com>
2024-03-05 00:14:32 +0200

    fixed hw6

--- hw6/moveListEl.py
+++ hw6/moveListEl.py
@@ -4,10 +4,7 @@
 example_list = [12, 3, 4, 10, 8]
 
 if len(example_list):
-    first_el = example_list[0]
-    last_el = example_list[-1]
-
-    example_list[0] = last_el
-    example_list[-1] = first_el
+    last_el = example_list.pop()
+    example_list.insert(0, last_el)
 
 print(example_list)
