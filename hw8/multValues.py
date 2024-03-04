f6a56c1fd8dec00274ab71ef4b44ef7b462789ba
iKot <sergey.savchuk91@gmail.com>
2024-03-05 00:24:06 +0200

    fixed hw8

--- hw8/multValues.py
+++ hw8/multValues.py
@@ -16,6 +16,6 @@ example_list = []
 result = 0
 
 if len(example_list):
-    result = sum([num for i, num in enumerate(example_list) if not i % 2]) * max(example_list)
+    result = sum([num for i, num in enumerate(example_list) if not i % 2]) * example_list[-1]
 
 print(result)
