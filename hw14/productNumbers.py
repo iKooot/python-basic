7269899c7bc65e936f4c88fbb72c2e6d8387c775
iKot <sergey.savchuk91@gmail.com>
2024-03-09 10:58:39 +0200

    fixed hw14

--- hw14/productNumbers.py
+++ hw14/productNumbers.py
@@ -1,26 +1,24 @@
 user_input = input("Enter a number:\n")
+# user_input = '999' # -> 2
+# user_input = '1000' # -> 0
+# user_input = '423' # -> 8
+# user_input = '33' # -> 9
+# user_input = '25' # -> 0
+# user_input = '1' # -> 1
+# user_input = '854' # -> 0
 
 result = None
 do_calculation = True
 
 while do_calculation:
-    if result is None:
-        temp_result = None
-        for number in [int(number) for number in user_input]:
-            if temp_result is None:
-                temp_result = number
-                continue
-            temp_result *= number
-        result = temp_result
+    temp_result = None
+    for number in [int(number) for number in result or user_input]:
+        if temp_result is None:
+            temp_result = number
+            continue
+        temp_result *= number
+    result = str(temp_result)
 
-    if result >= 10:
-        temp_result = None
-        for number in [int(digit) for digit in str(result)]:
-            if temp_result is None:
-                temp_result = number
-                continue
-            temp_result *= number
-        result = temp_result
-    else:
+    if len(result) == 1:
         print(result)
         do_calculation = False
