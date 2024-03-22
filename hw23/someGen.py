c8eacbede53dc08b62f579f3fc579675e568574d
iKot <sergey.savchuk91@gmail.com>
2024-03-22 23:52:17 +0200

    fixed some stuff

--- hw23/someGen.py
+++ hw23/someGen.py
@@ -14,7 +14,7 @@ def some_gen(begin: int, end: int, func: callable) -> Generator[int, None, None]
         count += 1
 
 
-gen = some_gen(2, 4, pow)
+gen = some_gen(2, 4, my_pow)
 
 assert isgenerator(gen) == True, 'Test1'
 assert list(gen) == [2, 4, 16, 256], 'Test2'
